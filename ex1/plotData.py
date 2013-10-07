cb1d54e90d508326034d0302e6eed0d02a1f6905
Samarth Bansal <bansalsamarth@yahoo.com>
2013-10-07 20:04:34 +0530

    Added plotData.py : Script to visualize data

---
+++ ex1/plotData.py
@@ -0,0 +1,19 @@
+#ex1-mlclass plotData.py
+
+#Before starting on any task, it is often useful to understand the data by
+#visualizing it. For this dataset, you can use a scatter plot to visualize the
+#data, since it has only two properties to plot (profit and population).
+
+import numpy as np
+import pylab as pl
+
+data = np.genfromtxt('ex1data.csv', delimiter = ',')
+dataT = data.transpose()
+
+X = dataT[0]
+y = dataT[1]
+
+pl.plot(X, y, 'ro')
+pl.xlabel('Population of City in 10,000s')
+pl.ylabel('Profit in $10,000s')
+pl.show()
